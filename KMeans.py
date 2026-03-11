cb76b45f82d62babcd7008415f0c57e8b16472cb
Aidan Updyke <aupdyke@bgsu.edu>
2026-03-11 14:04:04 -0400

    Initial KMeans runthrough

--- KMeans.py
+++ KMeans.py
@@ -1,2 +1,38 @@
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 import numpy as np
+import pandas as pd
+
+data = pd.read_csv(
+    "Data/HIGGS.csv.gz",
+    header = None,
+    compression = "gzip",
+    nrows = 1000000
+)
+
+# Need to seperate the labels and the features
+y = data.iloc[:,0].astype(int) #Signals
+X = data.iloc[:,1:] #Features
+
+scaler = StandardScaler()
+scaled_X = scaler.fit_transform(X)
+
+
+kmeans = MiniBatchKMeans(
+    n_clusters = 2,
+    batch_size = 10000,
+    n_init = 10,
+    random_state = 42
+)
+
+#kmeans.fit(scaled_X)
+
+clusters = kmeans.fit_predict(scaled_X)
+
+# Find Algorithm Accuracy
+acc1 = accuracy_score(y, clusters)
+acc2 = accuracy_score(y, 1 - clusters)
+
+print("Best clustering accuracy:", max(acc1, acc2))
+
